658211892d042298dfe53d0e46818b713ef47fbf
justEres <erik.tschoepe@gmail.com>
2024-09-09 21:01:17 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,321 @@
+function get_data(){
+    return [
+        {
+            "id": "01",
+            "werkname": "Eine kleine Nachtmusik, 1. Satz",
+            "komponist": "Wolfgang Amadeus Mozart",
+            "epoche": "Klassik"
+        },
+        {
+            "id": "02",
+            "werkname": "Badinerie (aus: Suite N°2 in h-moll)",
+            "komponist": "Johann Sebastian Bach",
+            "epoche": "Barock"
+        },
+        {
+            "id": "03",
+            "werkname": "Zadock the Priest",
+            "komponist": "Georg Friedrich Händel",
+            "epoche": "Barock"
+        },
+        {
+            "id": "04",
+            "werkname": "Ungarischer Tanz N°5",
+            "komponist": "Johannes Brahms",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "05",
+            "werkname": "Rachearie (aus: Zauberflöte)",
+            "komponist": "Wolfgang Amadeus Mozart",
+            "epoche": "Klassik"
+        },
+        {
+            "id": "06",
+            "werkname": "Walzer (aus: Jazz-Suite N°2)",
+            "komponist": "Dimitri Schostakowitsch",
+            "epoche": "Musik des 20. Jhd."
+        },
+        {
+            "id": "07",
+            "werkname": "Morgenstimmung (aus: Peer Gynt Suite)",
+            "komponist": "Edvard Grieg",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "08",
+            "werkname": "Für Elise",
+            "komponist": "Ludwig van Beethoven",
+            "epoche": "Klassik"
+        },
+        {
+            "id": "09",
+            "werkname": "Hochzeitsmarsch (aus: Sommernachtstraum)",
+            "komponist": "F. Mendelssohn-Bartholdy",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "10",
+            "werkname": "Symphonie N°5, 1. Satz (Schicksalssinfonie)",
+            "komponist": "Ludwig van Beethoven",
+            "epoche": "Klassik"
+        },
+        {
+            "id": "11",
+            "werkname": "Halleluja (aus: Der Messias)",
+            "komponist": "Georg Friedrich Händel",
+            "epoche": "Barock"
+        },
+        {
+            "id": "12",
+            "werkname": "O Fortuna (aus: Carmina Burana)",
+            "komponist": "Carl Orff",
+            "epoche": "Musik des 20. Jhd."
+        },
+        {
+            "id": "13",
+            "werkname": "Nessun Dorma (aus: Turandot)",
+            "komponist": "Giacomo Puccini",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "14",
+            "werkname": "Präludium (aus: Te Deum)",
+            "komponist": "Marc Antoine Charpentier",
+            "epoche": "Barock"
+        },
+        {
+            "id": "15",
+            "werkname": "Gymnopedie N°1",
+            "komponist": "Eric Satie",
+            "epoche": "Musik des 20. Jhd."
+        },
+        {
+            "id": "16",
+            "werkname": "Klavierkonzert N°1, 1. Satz",
+            "komponist": "Peter Tschaikowsky",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "17",
+            "werkname": "La Rejouissance (aus: Feuerwerksmusik)",
+            "komponist": "Georg Friedrich Händel",
+            "epoche": "Barock"
+        },
+        {
+            "id": "18",
+            "werkname": "Rhapsodie (über ein Thema von Paganini)",
+            "komponist": "Sergej Rachmaninow",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "19",
+            "werkname": "Schlusschor (aus 9. Sinfonie)",
+            "komponist": "Ludwig van Beethoven",
+            "epoche": "Klassik"
+        },
+        {
+            "id": "20",
+            "werkname": "Rule Britannia",
+            "komponist": "Thomas Augustin Arne",
+            "epoche": "Klassik"
+        },
+        {
+            "id": "21",
+            "werkname": "Also sprach Zarathustra",
+            "komponist": "Richard Strauss",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "22",
+            "werkname": "Lacrimosa (aus: Requiem)",
+            "komponist": "Wolfgang Amadeus Mozart",
+            "epoche": "Klassik"
+        },
+        {
+            "id": "23",
+            "werkname": "Clair de lune (aus: Bergamasque-Suite)",
+            "komponist": "Claude Débussy",
+            "epoche": "Impressionismus"
+        },
+        {
+            "id": "24",
+            "werkname": "Sinfonie g-moll, 1. Satz",
+            "komponist": "Wolfgang Amadeus Mozart",
+            "epoche": "Klassik"
+        },
+        {
+            "id": "25",
+            "werkname": "Liebestraum",
+            "komponist": "Franz Liszt",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "26",
+            "werkname": "Marsch N°1 (aus: Pomp & Circumstances)",
+            "komponist": "Edvard Elgar",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "27",
+            "werkname": "Brandenburgisches Konzert N°2",
+            "komponist": "Johann Sebastian Bach",
+            "epoche": "Barock"
+        },
+        {
+            "id": "28",
+            "werkname": "Klavierkonzert N°2, 2. Satz",
+            "komponist": "Frédéric Chopin",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "29",
+            "werkname": "Adagio g-moll",
+            "komponist": "Tomaso Albioni",
+            "epoche": "Barock"
+        },
+        {
+            "id": "30",
+            "werkname": "Walkürenritt (aus: Die Walküre)",
+            "komponist": "Richard Wagner",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "31",
+            "werkname": "Blumenwalzer (aus: Der Nussknacker)",
+            "komponist": "Peter Tschaikowsky",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "32",
+            "werkname": "Suite N°2, 12. Satz (aus: Wassermusik)",
+            "komponist": "Georg Friedrich Händel",
+            "epoche": "Barock"
+        },
+        {
+            "id": "33",
+            "werkname": "Kaiserwalzer",
+            "komponist": "Johann Strauss",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "34",
+            "werkname": "Klavierkonzert A-Dur, KV 488, 3. Satz",
+            "komponist": "Wolfgang Amadeus Mozart",
+            "epoche": "Klassik"
+        },
+        {
+            "id": "35",
+            "werkname": "Boléro",
+            "komponist": "Maurice Ravel",
+            "epoche": "Impressionismus"
+        },
+        {
+            "id": "36",
+            "werkname": "Der Schwan (aus: Carnaval des animaux)",
+            "komponist": "Camille Saint-Saens",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "37",
+            "werkname": "Träumerei (aus: Kinderszenen)",
+            "komponist": "Robert Schumann",
+            "epoche": "Romantik"
+        },
+        {
+            "id": "38",
+            "werkname": "Violinkonzert, 3. Satz Rondo",
+            "komponist": "Ludwig van Beethoven",
+            "epoche": "Klassik"
+        },
+        {
+            "id": "39",
+            "werkname": "Ché faro senza Euridice (aus: Orpheus)",
+            "komponist": "Willibald Gluck",
+            "epoche": "Barock"
+        },
+        {
+            "id": "40",
+            "werkname": "Jerusalem",
+            "komponist": "Hubert Parry",
+            "epoche": "Romantik"
+        }
+    ]
+}
+function get_komponisten(){
+    return [
+        "Camille Saint-Saens",
+        "Carl Orff",
+        "Claude Débussy",
+        "Dimitri Schostakowitsch",
+        "Edvard Elgar",
+        "Edvard Grieg",
+        "Eric Satie",
+        "F. Mendelssohn-Bartholdy",
+        "Franz Liszt",
+        "Frédéric Chopin",
+        "Georg Friedrich Händel",
+        "Giacomo Puccini",
+        "Hubert Parry",
+        "Johann Sebastian Bach",
+        "Johann Strauss",
+        "Johannes Brahms",
+        "Ludwig van Beethoven",
+        "Marc Antoine Charpentier",
+        "Maurice Ravel",
+        "Peter Tschaikowsky",
+        "Richard Strauss",
+        "Richard Wagner",
+        "Robert Schumann",
+        "Sergej Rachmaninow",
+        "Thomas Augustin Arne",
+        "Tomaso Albioni",
+        "Willibald Gluck",
+        "Wolfgang Amadeus Mozart",
+        "Petermän" //nicht hinterfragen
+      ]
+}
+function get_stücke(){
+    return [
+        "Eine kleine Nachtmusik, 1. Satz",
+        "Badinerie (aus: Suite N°2 in h-moll)",
+        "Zadock the Priest",
+        "Ungarischer Tanz N°5",
+        "Rachearie (aus: Zauberflöte)",
+        "Walzer (aus: Jazz-Suite N°2)",
+        "Morgenstimmung (aus: Peer Gynt Suite)",
+        "Für Elise",
+        "Hochzeitsmarsch (aus: Sommernachtstraum)",
+        "Symphonie N°5, 1. Satz (Schicksalssinfonie)",
+        "Halleluja (aus: Der Messias)",
+        "O Fortuna (aus: Carmina Burana)",
+        "Nessun Dorma (aus: Turandot)",
+        "Präludium (aus: Te Deum)",
+        "Gymnopedie N°1",
+        "Klavierkonzert N°1, 1. Satz",
+        "La Rejouissance (aus: Feuerwerksmusik)",
+        "Rhapsodie (über ein Thema von Paganini)",
+        "Schlusschor (aus 9. Sinfonie)",
+        "Rule Britannia",
+        "Also sprach Zarathustra",
+        "Lacrimosa (aus: Requiem)",
+        "Clair de lune (aus: Bergamasque-Suite)",
+        "Sinfonie g-moll, 1. Satz",
+        "Liebestraum",
+        "Marsch N°1 (aus: Pomp & Circumstances)",
+        "Brandenburgisches Konzert N°2",
+        "Klavierkonzert N°2, 2. Satz",
+        "Adagio g-moll",
+        "Walkürenritt (aus: Die Walküre)",
+        "Blumenwalzer (aus: Der Nussknacker)",
+        "Suite N°2, 12. Satz (aus: Wassermusik)",
+        "Kaiserwalzer",
+        "Klavierkonzert A-Dur, KV 488, 3. Satz",
+        "Boléro",
+        "Der Schwan (aus: Carnaval des animaux)",
+        "Träumerei (aus: Kinderszenen)",
+        "Violinkonzert, 3. Satz Rondo",
+        "Ché faro senza Euridice (aus: Orpheus)",
+        "Jerusalem"
+      ]
+}
---
+++ script.js
@@ -0,0 +1,137 @@
+
+const data = get_data();
+const komponisten = get_komponisten();
+const stücke = get_stücke();
+
+fillKomponistenDataList()
+fillStückeDataList()
+
+
+
+//check()
+let min = 0;
+let max = 39;
+let rand = false;
+let currentSongId = min;
+
+
+
+function newSong(){
+
+
+    if (rand == true){
+        id = min + Math.floor(Math.random()* (max-min))
+    }else{
+        if (currentSongId == max){
+            id = 0;
+        }
+        id = currentSongId + 1
+    }
+
+    
+
+    currentSongId = id;
+    let path = "/music/id/" + data[id].id + ".mp3"
+    let audiosrc = document.createElement("source");
+    audiosrc.src = path;
+    audiosrc.type = "audio/mpeg";
+    audiosrc.id = "audiosrc";
+    document.getElementById("audiosrc").replaceWith(audiosrc)
+    document.getElementById("audioplayer").load()
+}
+
+function go(){
+    document.getElementById("landing").style.display = "none";
+    document.getElementById("quiz").style.display = "block";
+    min = document.getElementById("rangeMin").value - 1;
+    max = document.getElementById("rangeMax").value - 1;
+    rand = document.getElementById("random").checked;
+    console.log(rand)
+    currentSongId = -1;
+    newSong()
+    resetInputs()
+}
+
+function resetInputs(){
+    let werkname = document.getElementById("werknameInput").value = "";
+    document.getElementById("werknameInput").style.border = "1px solid var(--border)"
+    document.getElementById("werknameLabel").innerHTML = "";
+    let komponist = document.getElementById("komponistenInput").value = "";
+    document.getElementById("komponistenInput").style.border = "1px solid var(--border)"
+    document.getElementById("komponistenLabel").innerHTML = "";
+    let epoche = document.getElementById("epoche").value = "none";
+    document.getElementById("epoche").style.border = "1px solid var(--border)"
+    document.getElementById("epochenLabel").innerHTML = "";
+    document.getElementById("checkButton").innerHTML = "CHECK"
+
+
+}
+
+
+function check(){
+    if (document.getElementById("checkButton").innerHTML == "WEITER"){
+        newSong();
+        resetInputs();
+        return;
+    }
+    let werkname = document.getElementById("werknameInput").value;
+    let komponist = document.getElementById("komponistenInput").value;
+    let epoche = document.getElementById("epoche").value;
+
+   
+    let stück = data[currentSongId]
+    if (werkname == stück.werkname){
+        document.getElementById("werknameInput").style.border = "3px solid lime";
+    } 
+    else{
+        document.getElementById("werknameInput").style.border = "3px solid red"; 
+        document.getElementById("werknameLabel").innerHTML = stück.werkname;
+    }
+    if (komponist == stück.komponist){
+        document.getElementById("komponistenInput").style.border = "3px solid lime";
+    }
+    else{
+        document.getElementById("komponistenInput").style.border = "3px solid red";
+    }   document.getElementById("komponistenLabel").innerHTML = stück.komponist;
+    if (epoche == stück.epoche){
+        document.getElementById("epoche").style.border = "3px solid lime";
+    }
+    else{
+        document.getElementById("epoche").style.border = "3px solid red";
+        document.getElementById("epochenLabel").innerHTML = stück.epoche;
+    }
+
+    document.getElementById("checkButton").innerHTML = "WEITER"
+    
+}
+//old: 1px solid var(--border)
+
+
+function fillKomponistenDataList(){
+    
+    i = 0,
+    len = komponisten.length;
+    let dl = document.createElement("datalist");
+    dl.id = "komponistenDataList";
+    for (;i<len;i+=1){
+        let option = document.createElement("option");
+        option.value = komponisten[i];
+        dl.appendChild(option);
+    }
+    document.getElementById("quiz").appendChild(dl);
+
+}
+function fillStückeDataList(){
+    
+    i = 0,
+    len = stücke.length;
+    let dl = document.createElement("datalist");
+    dl.id = "stückeDataList";
+    for (;i<len;i+=1){
+        let option = document.createElement("option");
+        option.value = stücke[i];
+        dl.appendChild(option);
+    }
+    document.getElementById("quiz").appendChild(dl);
+
+}
